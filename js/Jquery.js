678677ca483ad9f14bccc4a76fc41e5fe367f625
bastianmadrid <81173888+bastianmadrid@users.noreply.github.com>
2022-04-26 10:42:41 -0400

    valdaciones correctas y intento de json

--- js/Jquery.js
+++ js/Jquery.js
@@ -9,6 +9,13 @@ $(document).ready(function() {
                 required: true,
                 email: true
             },
+            pass1: {
+                required: true,
+
+            },
+            pass2: {
+                required: true,
+            }
 
         },
         messages: {
@@ -20,29 +27,35 @@ $(document).ready(function() {
                 required: "Debe ingresar el correo",
                 email: "El correo no es valido"
             },
+            pass1: {
+                required: "ingrese contraseña"
+            },
+            pass2: {
+                required: "repita contraseña"
+            },
 
         }
     });
-    
-        $('#pass2').keyup(function(e) {
-            var pass1 = $('#pass1').val();
-            var pass2 = $('#pass2').val();
-            if (pass1 == pass2) {
-                $('#error2').text("Coinciden").css("color", "green");
 
-            } else {
-                $('#error2').text("No coinciden").css("color", "red");
+    $('#pass2').keyup(function(e) {
+        var pass1 = $('#pass1').val();
+        var pass2 = $('#pass2').val();
+        if (pass1 == pass2) {
+            $('#error2').text("Coinciden").css("color", "green");
 
-            }
-            if (pass2 == "") {
-                $('#error2').text("No se puede dejar en blanco").css("color", "red");
+        } else {
+            $('#error2').text("No coinciden").css("color", "red");
 
-            }
-            if (pass1 == "") {
-                $('#error1').text("No se puede dejar en blanco").cssBordercolor("color", "red");
-            } else {
-                $('#error1').text("");
-            }
+        }
+        if (pass2 == "") {
+            $('#error2').text("No se puede dejar en blanco").css("color", "red");
 
-        });
+        }
+        if (pass1 == "") {
+            $('#error1').text("No se puede dejar en blanco").cssBordercolor("color", "red");
+        } else {
+            $('#error1').text("");
+        }
+
+    });
 });
