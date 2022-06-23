feeaa52094cc98e07ebe7269ff06491fcc1853be
bastianmadrid <81173888+bastianmadrid@users.noreply.github.com>
2022-06-23 05:02:48 -0400

    Delete sporting-house-proyecto-master-master directory

--- sporting-house-proyecto-master-master/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import producto, usuario, zapatillas, bicileta, equipo
-# Register your models here.
-
-admin.site.register(zapatillas)
-admin.site.register(bicileta)
-admin.site.register(equipo)
-admin.site.register(usuario)
-admin.site.register(producto)
--- sporting-house-proyecto-master-master/rest_usuario/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from core.models import usuario
-
-class usuarioSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = usuario
-        fields = ['nombre','contrasena','correo']
